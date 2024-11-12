30324fd2a0bd2ee272e0c4b30be7ae72c615b0e8
Al Alibrahim <aalibr2@uic.edu>
2024-11-11 21:15:43 -0600

    Added scripts folder

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const searchButton = document.getElementById("search-button");
+    const gameSearchInput = document.getElementById("game-search");
+    const resultSection = document.getElementById("result");
+    const defaultGamesSection = document.getElementById("default-games");
+    const discoverSection = document.getElementById("discover-section");
+    const profileSection = document.getElementById("profile-section");
+    const discoverButton = document.getElementById("discover-button");
+    const gamesButton = document.getElementById("games-button");
+    const profileButton = document.getElementById("profile-button");
+
+})
